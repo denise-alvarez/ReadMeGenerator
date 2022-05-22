cbd0d4bd6b4fd10fb9f299c9bb0f98816c12029a
denise-alvarez <denisealvarez3@gmail.com>
2022-05-21 19:02:39 -0700

    add

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,13 @@
 // TODO: Include packages needed for this application
+const inq = require ('inquirer');
+const chalk = require('chalk');
+const fs = require('fs');
+const questions = [
 
-// TODO: Create an array of questions for user input
-const questions = [];
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function createReadme () {
+    fs.writeFileSync('./README.md', 'testing')
+}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+createReadme()
