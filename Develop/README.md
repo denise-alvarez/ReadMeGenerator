e3a87a40b798e452807656fd92600e09d34f1f7d
denise-alvarez <denisealvarez3@gmail.com>
2022-05-21 22:21:41 -0700

    update js

--- Develop/README.md
+++ Develop/README.md
@@ -1,1 +1,20 @@
-this is the arguments: 😄
+
+    # ReadMeGenerator
+    ## Project title
+    Creating a high quality Readme 
+    ## Description
+    Github
+    ## Installation Instructions
+    This application is used to be able to easily create a ReadMe
+    ## Usage
+    
+    ## License Used
+    none
+    ## Guideline Contributions
+    https://denise-alvarez.github.io/ReadMeGenerator/
+    ## Deployed Link
+    denise-alvarez
+    ## Github Username
+    denisealvarez3@gmail.com
+    ## Email
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,64 +1,93 @@
-const inq = require('inquirer');
-const chalk = require('chalk');
-const fs = require('fs');
-const { create } = require('domain');
+const inq = require("inquirer");
+const chalk = require("chalk");
+const fs = require("fs");
+
 // Questions to ask the user
-const question = [
-{
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?',
-},
-{
-    type: 'input',
-    name: 'description',
-    message: 'What is your project about?',
-},
-{
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is your project all about?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do you install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is your application used for?",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "What kind of license is used?",
+    choices: ["BSD", "MIT", "GPL"],
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "What are the guidelines to contributing?",
+  },
+  {
+    type: "input",
+    name: "deployedLink",
+    message: "What is the link to your deployed project?",
+  },
+  {
+    type: "input",
+    name: "githubusername",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+];
 
-    type: 'input',
-    name: 'installation',
-    message: 'How do you install your project?',
-},
+function createReadme(answers) {
+  fs.writeFileSync(
+    "./README.md",
+    `
+    # ${answers.title}
+    ## Project title
+    ${answers.description}
+    ## Description
+    ${answers.installation}
+    ## Installation Instructions
+    ${answers.usage}
+    ## Usage
+    ${answers.license}
+    ## License Used
+    ${answers.contribute}
+    ## Guideline Contributions
+    ${answers.deployedLink}
+    ## Deployed Link
+    ${answers.githubusername}
+    ## Github Username
+    ${answers.email}
+    ## Email
+  `
+  );
+}
 
-{
-    type: 'input',
-    message: 'What is your application used for?',
-    name: 'usage',
-},
-{
-    type: 'checkbox',
-    message: 'What kind of license is used?',
-    name: 'license',
-    choices: ['BSD', 'MIT', 'GPL'],
-},
-{
-    type: 'input',
-    message: 'What are the guidelines to contributing?',
-    name: 'contribute',
-},
-{
-
-    type: 'input',
-    name: 'deployedLink',
-    message: 'What is the link to your deployed project?',
-},
-{
-
-    type: 'input',
-    name: 'githubusername',
-    message: 'What is your GitHub username?',
-},
-{
-
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address?',
-},
-]
-
-function createReadme(args) {
-    fs.writeFileSync ('./README.md' ,`this is the arguments: ${args}`)
-} 
-
-createReadme('😄');
+inq
+  .prompt(questions)
+  .then((answers) => {
+    createReadme(answers);
+    console.log(chalk.green("😍 Successfully created README.md"));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.error("😭 Prompts could not be rendered in current environment!");
+    } else {
+      console.error(`Something went wrong!`, error);
+    }
+  });
